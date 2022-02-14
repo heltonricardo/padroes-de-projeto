ae19451f7aba66d4a54f1a5f6aa1e7e6b6daaab9
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-02-14 20:15:35 -0300

    implementacao 03 do factory

---
+++ src/main/java/io/github/heltonricardo/creational_patterns/factory/implementation_03/Client3Factory.java
@@ -0,0 +1,19 @@
+package src.main.java.io.github.heltonricardo.creational_patterns.factory.implementation_03;
+
+import src.main.java.io.github.heltonricardo.creational_patterns.factory.implementation_03.simpleFactory.IPhoneFactory;
+import src.main.java.io.github.heltonricardo.creational_patterns.factory.model.IPhone;
+
+public class Client3Factory {
+
+    public static void main(String[] args) {
+
+        System.out.println("### Ordering an iPhone X Standard");
+        IPhone iPhone1 = IPhoneFactory.orderIPhone("X", "standard");
+        System.out.println(iPhone1);
+
+        System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
+        IPhone iPhone2 = IPhoneFactory.orderIPhone("11", "highEnd");
+        System.out.println(iPhone2);
+    }
+
+}
