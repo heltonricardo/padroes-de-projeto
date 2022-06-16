42ce1c140f22613d9ca9da57bf5995cc6d12d0d9
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-06-16 12:29:30 -0300

    implementacao 03 do prototype

---
+++ src/main/java/io/github/heltonricardo/creational_patterns/prototype/implementation_03/ClientPrototype3.java
@@ -0,0 +1,31 @@
+package src.main.java.io.github.heltonricardo.creational_patterns.prototype.implementation_03;
+
+import src.main.java.io.github.heltonricardo.creational_patterns.prototype.implementation_03.model.Address;
+import src.main.java.io.github.heltonricardo.creational_patterns.prototype.implementation_03.model.User;
+
+public class ClientPrototype3 {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        User originalUser = new User("Ana", 20, new Address("A street", 1));
+
+        User clonedUser1 = originalUser.clone();
+
+        User clonedUser2 = originalUser
+                .cloneBuilder()
+                .name("Ivo")
+                .build();
+
+        User clonedUser3 = clonedUser2
+                .cloneBuilder()
+                .name("Ada")
+                .age(30)
+                .address(new Address("B street", 2))
+                .build();
+
+        System.out.println(originalUser);
+        System.out.println(clonedUser1);
+        System.out.println(clonedUser2);
+        System.out.println(clonedUser3);
+    }
+}
---
+++ src/main/java/io/github/heltonricardo/creational_patterns/prototype/implementation_03/model/User.java
@@ -0,0 +1,60 @@
+package src.main.java.io.github.heltonricardo.creational_patterns.prototype.implementation_03.model;
+
+public class User implements Cloneable {
+
+    public final String name;
+    public final Integer age;
+    public final Address address;
+
+    public User(String name, Integer age, Address address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", age=" + age + ", address=" + address + "]";
+    }
+
+    @Override
+    public User clone() throws CloneNotSupportedException {
+        Address clonedAddress = address.clone();
+        return new User(name, age, clonedAddress);
+    }
+
+    public Builder cloneBuilder() {
+        return new Builder(name, age, address);
+    }
+
+    public static class Builder {
+        private String name;
+        private Integer age;
+        private Address address;
+
+        private Builder(String name, Integer age, Address address) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder age(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder address(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public User build() {
+            return new User(name, age, address);
+        }
+    }
+}
