b1d50ce9c51fbaa1bfd9fabead9f81e0ba5e6473
heltonricardo <50843386+heltonricardo@users.noreply.github.com>
2023-03-16 14:17:05 -0300

    refactor: arquivos do padrao adapter

--- src/main/java/info/helton/structural_patterns/adapter/implementation_01/Client.java
+++ src/main/java/info/helton/structural_patterns/adapter/implementation_01/Client.java
@@ -9,25 +9,27 @@ import info.helton.structural_patterns.adapter.implementation_01.devices.TV;
 public class Client {
 
 	public static void main(String[] args) {
+		final var RAINBOW = "Cat and rainbow";
+		final var SONG = "Nyan cat song";
+
 		Computer pc = new Computer();
 		TV tv = new TV();
 		pc.connectPort(tv);
-		pc.sendImageAndSound("Cat and rainbow", "Nyan cat song");
+		pc.sendImageAndSound(RAINBOW, SONG);
 
 		System.out.println("------ Monitor ----------");
 
 		Computer pc2 = new Computer();
 		OldMonitor monitor = new OldMonitor();
-		// pc2.connectPort(monitor.getConnector()); //Don't work! That's the reason why
-		// we create the adapter
+		// pc2.connectPort(monitor.getConnector()); //Don't work! That's why we create the adapter
 		pc2.connectPort(new HDMIToVGAAdapter(monitor));
-		pc2.sendImageAndSound("Cat and rainbow", "Nyan cat song");
+		pc2.sendImageAndSound(RAINBOW, SONG);
 
 		System.out.println("------ Monitor Class Adapter ----------");
 
 		Computer pc3 = new Computer();
 		HDMIToOldMonitorAdapter monitorAdaptee = new HDMIToOldMonitorAdapter();
 		pc3.connectPort(monitorAdaptee);
-		pc3.sendImageAndSound("Cat and rainbow", "Nyan cat song");
+		pc3.sendImageAndSound(RAINBOW, SONG);
 	}
 }
--- src/main/java/info/helton/structural_patterns/adapter/implementation_01/intefaces/HDMI.java
+++ src/main/java/info/helton/structural_patterns/adapter/implementation_01/intefaces/HDMI.java
@@ -2,5 +2,6 @@ package info.helton.structural_patterns.adapter.implementation_01.intefaces;
 
 public interface HDMI {
 	void setImage(String image);
+
 	void setSound(String sound);
 }
